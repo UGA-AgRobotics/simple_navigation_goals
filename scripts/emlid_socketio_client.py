516cc6dd56010e6c9298c6b3f495956301c448ae
popenc <i.nickpope@gmail.com>
2018-06-12 12:13:15 -0400

    rosifying the emlid socketio python client

--- scripts/emlid_socketio_client.py
+++ scripts/emlid_socketio_client.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python
+
 from socketIO_client import SocketIO, LoggingNamespace
 import logging
 import serial  # NOTE: This is pySerial library and not serial (i.e., )
 import time
 import sys
+import roslib
+import rospy
 
 
 
 class EmlidSocketIOClient:
 
-	def __init__(self, reach_ip=None, reach_port=None, arduino_path=None, arduino_baud=None):
+	def __init__(self, reach_ip=None, reach_port=None, arduino_port=None, arduino_baud=None):
 
 		# To set DEBUG for more verbose messages for troubleshooting, 
 		# uncomment below logging lines:
@@ -17,11 +21,11 @@ class EmlidSocketIOClient:
 
 		self.reach_ip = reach_ip or '192.168.131.200'
 		self.reach_port = reach_port or 80
-		self.arduino_path = arduino_path or '/dev/ttyACM0'
+		self.arduino_port = arduino_port or '/dev/ttyACM0'
 		self.arduino_baud = arduino_baud or 9600
 
 		print("Reach IP: {}, Reach Port: {}".format(self.reach_ip, self.reach_port))
-		print("Arduino serial path: {}, Arduino baud: {}".format(self.arduino_path, self.arduino_baud))
+		print("Arduino serial path: {}, Arduino baud: {}".format(self.arduino_port, self.arduino_baud))
 
 		self.reach_keys = [
 			'solution status',
@@ -32,7 +36,7 @@ class EmlidSocketIOClient:
 
 		self.status_options = ['fix', 'float', 'single', '-']
 
-		self.arduino = serial.Serial(self.arduino_path, self.arduino_baud, timeout=0.2, write_timeout=0.2)  # initiate conn to arduino via serial
+		self.arduino = serial.Serial(self.arduino_port, self.arduino_baud, timeout=0.2, write_timeout=0.2)  # initiate conn to arduino via serial
 		time.sleep(2)  # waiting to ensure connection to arduino before writing to it..
 
 		self.connect_to_socketio_server()  # initiate conn to emlid's socketio server
@@ -109,16 +113,20 @@ if __name__ == '__main__':
 
 	_reach_ip = None
 	_reach_port = None
-	_arduino_path = None
+	_arduino_port = None
 	_arduino_baud = None
 
 	try:
 		_reach_ip = sys.argv[1]
 		_reach_port = sys.argv[2]
-		_arduino_path = sys.argv[3]
+		_arduino_port = sys.argv[3]
 		_arduino_baud = sys.argv[4]
 	except IndexError:
 		print("No inputs provided for reach ip or reach port, so using defaults..")
-		pass
+		print("Trying to use ROS get_param, assuming it's being run as ROS node instead of from terminal..")
+		_reach_ip = rospy.get_param('~REACH_IP')
+		_reach_port = rospy.get_param('~REACH_PORT')
+		_arduino_port = rospy.get_param('~ARDUINO_PORT')
+		_arduino_baud = rospy.get_param('~ARDUINO_BAUD')
 
-	emlidsock = EmlidSocketIOClient(_reach_ip, _reach_port, _arduino_path, _arduino_baud)
+	emlidsock = EmlidSocketIOClient(_reach_ip, _reach_port, _arduino_port, _arduino_baud)

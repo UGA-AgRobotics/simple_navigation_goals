c1ebf3abfcb46653fa4bcd8cb072ee9e1738ebc3
popenc <i.nickpope@gmail.com>
2018-06-22 11:27:36 -0400

    changed .env to be .sh file that runs export commands to set env vars

---
+++ config/red_rover_env.sh
@@ -0,0 +1,9 @@
+export ARDUINO_BAUD=9600
+export ARDUINO_SERIAL_PORT=/dev/ttyACM1
+export FLAG_FILE=/home/brad/Documents/ROS_Workspaces/simple_navigation_goals/src/simple_navigation_goals/goals/flags_2.json
+export PATH_FILE=/home/brad/Documents/ROS_Workspaces/simple_navigation_goals/src/simple_navigation_goals/courses/course_9_filled.json
+export REACH_BAUD=9600
+export REACH_IP=192.168.131.201
+export REACH_SERIAL_PORT=/dev/ttyACM0
+export ROS_MASTER_URI=http://192.168.131.11:11311
+export TEST_VAR=test

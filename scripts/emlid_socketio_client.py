c7a0bd778f77a69ba51686eac54f90387b2c1a08
popenc <i.nickpope@gmail.com>
2019-01-04 12:05:30 -0500

    added ar validation ratio, fix status, and connection status topics

--- scripts/emlid_socketio_client.py
+++ scripts/emlid_socketio_client.py
@@ -8,7 +8,7 @@ import sys
 import roslib
 import rospy
 import json
-from std_msgs.msg import String, Bool
+from std_msgs.msg import String, Bool, Float32
 from sensor_msgs.msg import NavSatFix
 
 
@@ -37,10 +37,10 @@ class EmlidSocketIOParameters:
 		self.response_example = {"receiver time mark count": "0", "ant type base": "", "solution status": "fix", "vel enu (m/s) base": "0.000,0.000,0.000", "time of receiver clock rover": "2018/09/05 18:07:34.800360200", "baseline length float (m)": "3.840", "# of rtcm messages rover": "", "time sys offset (ns)": "0.000,0.000,0.000,0.000", "# of average single pos base": "0", "pos xyz float std (m) rover": "0.006,0.012,0.009", "ant delta rover": "0.000 0.000 0.000", "# of rtcm messages corr": "", "accumulated time to run": "01:29:22.2", "cpu time for a cycle (ms)": "28", "# of rtcm messages base": "1004(5355),1005(1071),1007(1071),1012(5355),1019(536),1030(1071),1031(1071),1032(536),1033(1071),other3(1072)", "vel enu (m/s) rover": "-0.052,0.004,-0.053", "ant type rover": "", "pos xyz single (m) rover": "660552.040,-5173953.727,3659529.854", "\x1b[1mParameter": "Value\x1b[0m", "rtk server state": "run", "pos xyz fixed (m) rover": "660552.040,-5173953.727,3659529.854", "age of differential (s)": "0.793", "pos xyz fixed std (m) rover": "0.006,0.012,0.009", "# of all estimated states": "170", "processing cycle (ms)": "10", "ant delta base": "0.000 0.000 0.000", "# of valid satellites": "7", "rtklib version": "2.4.3 Emlid b28", "bytes in input buffer": "0,0", "# of input data corr": "obs(0),nav(0),gnav(0),ion(0),sbs(0),pos(0),dgps(0),ssr(0),err(0)", "# of input data rover": "obs(26805),nav(20),gnav(0),ion(1479),sbs(0),pos(0),dgps(0),ssr(0),err(0)", "pos xyz (m) base": "660552.459,-5173950.029,3659528.885", "ratio for ar validation": "46.156", "pos llh (deg,m) base": "35.23478873,-82.72446983,615.833", "missing obs data count": "0", "# of real estimated states": "9", "baseline length fixed (m)": "3.846", "# of satellites base": "13", "last time mark": "-", "# of input data base": "obs(5355),nav(19),gnav(0),ion(0),sbs(0),pos(3213),dgps(0),ssr(0),err(0)", "# of satellites rover": "9", "GDOP/PDOP/HDOP/VDOP": "2.3,2.0,1.1,1.7", "pos xyz float (m) rover": "660552.039,-5173953.721,3659529.851", "positioning mode": "kinematic", "pos llh single (deg,m) rover": "35.23477707,-82.72447955,619.345", "solution interval (s)": "0.200", "rtklib time mark count": "0"}
 
 		# Defined variables for emlid keys that are relevant to the rover's GPS:
-		self.rover_pos_key = 'pos llh single (deg,m) rover'
-		self.rover_status_key = 'solution status'
-		self.rover_ar_ratio = 'ratio for ar validation'
-		self.rover_baseline = 'baseline length float (m)'
+		self.pos_key = 'pos llh single (deg,m) rover'
+		self.status_key = 'solution status'
+		self.ar_ratio_key = 'ratio for ar validation'
+		self.baseline_key = 'baseline length float (m)'
 
 
 
@@ -72,7 +72,9 @@ class EmlidSocketIOClient(EmlidSocketIOParameters):
 		# Publishers:
 		self.solution_status_publisher = rospy.Publisher("/emlid_solution_status", String, queue_size=1)
 		self.gps_stop_publisher = rospy.Publisher("/stop_gps", Bool, queue_size=1)  # stop until GPS gets Fix again
-		self.gps_pos_publisher = rospy.Publiser("/fix", NavSatFix, queue_size=1)
+		self.gps_pos_publisher = rospy.Publisher("/fix", NavSatFix, queue_size=1)
+		self.connected_publisher = rospy.Publisher("/emlid_connected", Bool, queue_size=1)
+		self.ar_ratio_publisher = rospy.Publisher("/emlid_ar_ratio", Float32, queue_size=1)
 
 		rospy.sleep(2)
 
@@ -96,7 +98,8 @@ class EmlidSocketIOClient(EmlidSocketIOParameters):
 	def on_connect(self):
 		print("Connected to rover's Emlid Reach SocketIO server.")
 		print("Reach IP: {}, Reach Port: {}".format(self.emlid_ip, self.emlid_port))
-		self.is_connected = True
+		# self.is_connected = True
+		self.connected_publisher.publish(True)
 		self.msg_num = 0
 
 
@@ -105,7 +108,8 @@ class EmlidSocketIOClient(EmlidSocketIOParameters):
 		print("Emlid node's socketio connection disconnected. Sending message to stop rover on /stop_gps.")
 		self.gps_stop_publisher.publish(True)
 
-		self.is_connected = False
+		# self.is_connected = False
+		self.connected_publisher.publish(False)
 		self.msg_num = 0
 
 		print("Shutting down socketio client ROS node.")
@@ -116,9 +120,10 @@ class EmlidSocketIOClient(EmlidSocketIOParameters):
 
 
 	def on_reconnect(self):
-		print('reconnected.')
-		self.is_connected = True
+		print('Reconnected to Emlid SocketIO server.')
+		# self.is_connected = True
 		self.msg_num = 0
+		self.connected_publisher.publish(True)
 
 
 
@@ -136,34 +141,56 @@ class EmlidSocketIOClient(EmlidSocketIOParameters):
 			return
 
 		print("Message {}:".format(self.msg_num))
-		print("GPS Position: {}".format(emlid_data[self.rover_pos_key]))
-		print("GPS Status: {}".format(emlid_data[self.rover_status_key]))
-		print("GPS AR Validation Ratio: {}".format(emlid_data[self.rover_ar_ratio]))
+		print("GPS Position: {}".format(emlid_data[self.pos_key]))
+		print("GPS Status: {}".format(emlid_data[self.status_key]))
+		print("GPS AR Validation Ratio: {}".format(emlid_data[self.ar_ratio_key]))
 		self.msg_num += 1
 
-		self.publish_emlid_pos(emlid_data)
+		# Checks solution status, publishes true/false to /gps_stop topic: 
+		self.check_for_fix(emlid_data[self.status_key])
+
+		# Publishes GPS fix status:
+		self.solution_status_publisher.publish(emlid_data[self.status_key])
+
+		# Publishes GPS position as NavSatFix on /fix topic:
+		self.publish_emlid_pos(emlid_data[self.pos_key])
+
+		# Publishes GPS ar validation ratio:
+		self.publish_ar_ratio(emlid_data[self.ar_ratio_key])
+
+		# Publishes GPS connection status:
+		self.connected_publisher.publish(True)
 
 
 
-	def publish_emlid_pos(self, emlid_data):
+	def publish_ar_ratio(self, ar_ratio):
+		"""
+		Publishes AR validation ratio on /emlid_ar_ratio topic.
+		"""
+		try:
+			ar_ratio = float(ar_ratio)
+		except Exception:
+			pass
+		self.ar_ratio_publisher.publish(ar_ratio)
+
+
+
+	def publish_emlid_pos(self, pos_data):
 		"""
 		Parses the lat,lon,alt string into a NavSatFix object, 
 		and publishes to /fix topic.
 		"""
 
-		# Gets LLH values:
-		emlid_list = emlid_data.split(',')
-		emlid_lat = float(emlid_list[0])
-		emlid_lon = float(emlid_list[1])
-		emlid_alt = float(emlid_list[2])
+		# Gets LLH values as list (e.g., [lat, lon, alt]):
+		emlid_list = pos_data.split(',')
 
 		# Creates NavSatFix object:
 		navsat_pos = NavSatFix()
-		navsat_pos['latitude'] = emlid_lat
-		navsat_pos['longitude'] = emlid_lon
-		navsat_pos['altitude'] = emlid_alt
+		navsat_pos.latitude = float(emlid_list[0])
+		navsat_pos.longitude = float(emlid_list[1])
+		navsat_pos.altitude = float(emlid_list[2])
 
-		print("Publishing to /fix: {}".format(navsat_pos))
+		# print("Publishing to /fix: {}".format(navsat_pos))
 
 		# Publishes NavSatFix value to /fix topic:
 		self.gps_pos_publisher.publish(navsat_pos)
